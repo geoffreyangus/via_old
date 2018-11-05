536af7cd2a1435edb4ea07f736face6850349183
rdiehlmartinez <rdm@stanford.edu>
2018-11-04 18:34:06 -0800

    finished code for building prereq and biparte graph and added graphs folder which stores built graphs

--- data/build_graph.py
+++ data/build_graph.py
@@ -12,6 +12,7 @@ a student has attended Stanford.
 import click
 import snap
 import numpy as np
+from collections import Counter
 
 def load_matrix(type="sequence"):
     '''
@@ -45,19 +46,39 @@ def create_prereq_graph():
     largest_timestep = sequence_matrix.max()
 
     Graph = snap.TNGraph.New()
-    # Keeps track of all classes taken 1-timestep apart from each other for
-    # all students
-    delta_timestep = [[] for i in range(largest_timestep-1)]
+    # Keep track of all classes taken 1-timestep apart from each other
+    delta_timestep = []
     for i in range(matrix_shape[0]):
-        #student_sequence = np.argsort(sequence_matrix[i,:])
-        for j in range(matrix_shape[1]):
-            np.where(x == 0)[0]
-            # argsort: Returns the indices that would sort an array.
+        if i % 1000 == 0:
+            print("Processed {} number of rows ".format(i))
+        curr_sequence = sequence_matrix[i]
+        curr_max_timestep = int(curr_sequence.max())
+        past_classes = None
+        for timestep in range(1,curr_max_timestep+1):
+            if timestep == 1:
+                past_classes = np.where(curr_sequence == float(timestep))[0]
+                continue
+            current_classes = np.where(curr_sequence == float(timestep))[0]
+            # Appending past and current classes
+            for past_class in past_classes:
+                for current_class in current_classes:
+                    delta_timestep.append((past_class,current_class))
+            past_classes = current_classes
 
+    delta_timestep_counts = Counter(delta_timestep)
+    sorted_timestep_counts = sorted(delta_timestep_counts.items(),
+                                key=lambda x: x[1], reverse=True)
 
+    for timestep in sorted_timestep_counts[:300]:
+        class_tuple = timestep[0]
+        first_class,second_class = class_tuple
+        if not Graph.IsNode(first_class):
+            Graph.AddNode(first_class)
+        if not Graph.IsNode(second_class):
+            Graph.AddNode(second_class)
+        Graph.AddEdge(first_class,second_class)
 
-
-
+    return Graph
 
 def create_bipartite_graph():
     '''
@@ -99,8 +120,10 @@ def main(graph_type):
     Graph = None
     if graph_type == 'bipartite':
         Graph = create_bipartite_graph()
+        print("Created Bipartite graph")
     elif graph_type == 'prereq':
         Graph = create_prereq_graph()
+        print("Created Prereq graph")
     else:
         raise Exception('Invalid graph type provided: {}'.format(graph_type))
 
